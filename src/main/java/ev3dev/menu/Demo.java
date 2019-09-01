4adcf6aa6a9cac4d11276964f1f7e51f5655e68a
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-09-01 12:25:43 +0200

    Example testing the feature to take the control the main jar the execution when the jar loaded finish the logic.

--- src/main/java/ev3dev/menu/Demo.java
+++ src/main/java/ev3dev/menu/Demo.java
@@ -1,19 +1,31 @@
 package ev3dev.menu;
 
 import java.io.File;
+import java.util.stream.IntStream;
 import lejos.ev3.startup.JarMain;
 
 public class Demo {
 
     public static void main(String[] args) {
 
-        System.out.println("Running another jar");
+        System.out.println("Running another jar some times");
+
+        Demo demo = new Demo();
+
+        IntStream.rangeClosed(1, 5).boxed()
+                .forEach(i -> {
+                    demo.runAnotherJar();
+                    System.out.println("Jar process finished");
+                });
+    }
+
+    private void runAnotherJar() {
+
         try {
             File file = new File("./demo.jar");
             JarMain jar = new JarMain(file);
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }
-
     }
 }
